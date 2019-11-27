01d26dee420c09a1dd6e330a72d18aff57137ec6
deepthidh <55013427+deepthidh@users.noreply.github.com>
2019-11-27 23:39:43 +0530

    Update queue_main.c

--- queue_main.c
+++ queue_main.c
@@ -5,21 +5,26 @@
 
 int main()
 {
-   Queue queue = queue_new(10);
+   Queue queue = queue_new(3);
    Queue *q = &queue;
 
    Queue_Result res;
 
    q = queue_add(q,10,&res);
+    //queue_display(q);
    assert(q->count==1);
    assert(q->data[q->head-1] == 10);
-    q = queue_add(q,20,&res);
-     q = queue_add(q,30,&res);
-    assert(q->data[q->head-1]==30);
+	q = queue_add(q,20,&res);
+	//queue_display(q);
+   q = queue_add(q,30,&res);
+    //queue_display(q);
+   // assert(q->data[q->head-1]==30);
     assert(q->data[q->tail]==10);
 
     q = queue_delete(q,&res);
-    assert(res.data==10);
-    assert(q->data[q->tail]==20);
+    queue_display(q);
+    //assert(res.data==10);
+    //assert(q->data[q->tail]==20);
+    
     return 0;
 }
